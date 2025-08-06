f02a191f92673336b1863577012ec45cfeb9539a
haywire <haywire@phoenix.com>
2025-08-06 08:09:23 +0100

    fix performance - caching and early exits

--- Editor/AutoHookPropertyDrawer.cs
+++ Editor/AutoHookPropertyDrawer.cs
@@ -2,14 +2,22 @@ using System;
 using UnityEditor;
 using UnityEngine;
 
-
 [CustomPropertyDrawer(typeof(AutoHookAttribute))]
 public class AutoHookPropertyDrawer : PropertyDrawer
 {
+    private AutoHookAttribute _autoHookAttribute;
+    private AutoHookAttribute autoHookAttribute
+    {
+        get
+        {
+            if (_autoHookAttribute == null)
+                _autoHookAttribute = (AutoHookAttribute)attribute;
+            return _autoHookAttribute;
+        }
+    }
+
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        AutoHookAttribute autoHookAttribute = (AutoHookAttribute) attribute;
-
         if (autoHookAttribute.HideWhenFound && property.objectReferenceValue != null)
         {
             return 0;
@@ -20,31 +28,22 @@ public class AutoHookPropertyDrawer : PropertyDrawer
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        AutoHookAttribute autoHookAttribute = (AutoHookAttribute) attribute;
-
+        // Hide the property if HideWhenFound is true and the component is already found
         if (autoHookAttribute.HideWhenFound && property.objectReferenceValue != null)
         {
             return;
         }
 
-        if (autoHookAttribute.StopSearchWhenFound)
-        {
-            if (property.objectReferenceValue == null)
-            {
-                Component component = FindComponent(property);
-                property.objectReferenceValue = component;
-            }
-        }
-        else
+        if (property.objectReferenceValue == null)
         {
             Component component = FindComponent(property);
-
-            if (property.objectReferenceValue == null && component != null)
+            if (component != null)
             {
                 property.objectReferenceValue = component;
             }
         }
-
+        
+        // Disable the property if ReadOnlyWhenFound is true and the component is already found
         EditorGUI.BeginDisabledGroup(autoHookAttribute.ReadOnlyWhenFound && property.objectReferenceValue != null);
         EditorGUI.PropertyField(position, property, label);
         EditorGUI.EndDisabledGroup();
@@ -53,15 +52,13 @@ public class AutoHookPropertyDrawer : PropertyDrawer
     private Component FindComponent(SerializedProperty property)
     {
         SerializedObject root = property.serializedObject;
-        if (!(root.targetObject is Component))
+        if (!(root.targetObject is Component parent))
         {
             return null;
         }
 
         Type type = fieldInfo.FieldType;
-        Component parent = (Component) root.targetObject;
-        AutoHookAttribute autoHookAttribute = (AutoHookAttribute) attribute;
-
+        
         switch (autoHookAttribute.SearchArea)
         {
             case AutoHookSearchArea.Parent:
@@ -71,65 +68,32 @@ public class AutoHookPropertyDrawer : PropertyDrawer
             case AutoHookSearchArea.Scene:
                 return UnityEngine.Object.FindFirstObjectByType(type) as Component;
             case AutoHookSearchArea.FirstChild:
-                return FindComponentInFirstChild(parent.transform,type);
+                if (parent.transform.childCount > 0)
+                    return parent.transform.GetChild(0).GetComponent(type);
+                return null;
             case AutoHookSearchArea.Children:
-                return parent.GetComponentInChildren(type);
+                return parent.GetComponentInChildren(type, true);
             case AutoHookSearchArea.DirectChildrenOnly:
                 return FindComponentInDirectChildren(parent.transform, type);
             case AutoHookSearchArea.AllChildrenOnly:
-                return FindComponentInChildrenRecursive(parent.transform, type);
+                return parent.GetComponentInChildren(type, true);
             default:
                 return parent.GetComponent(type);
         }
     }
 
-    private Component GetComponentInChild(Transform parent, Transform child, Type type)
-    {
-        Component component = child.GetComponent(type);
-        if (component != null)
-            return component;
-        return null;
-    }
-    
-    private Component FindComponentInFirstChild(Transform parent, Type type)
-    {
-        if (parent.childCount == 0) return null;
-        var child = parent.GetChild(0);
-        Component component = GetComponentInChild(parent, child, type);
-        return component;
-    }
-
     private Component FindComponentInDirectChildren(Transform parent, Type type)
     {
-        foreach (Transform child in parent)
+        int childCount = parent.childCount;
+        for (int i = 0; i < childCount; i++)
         {
+            Transform child = parent.GetChild(i);
             Component component = child.GetComponent(type);
             if (component != null)
             {
                 return component;
             }
         }
-        return null;
-    }
-
-    private Component FindComponentInChildrenRecursive(Transform parent, Type type)
-    {
-        foreach (Transform child in parent)
-        {
-            Component component = child.GetComponent(type);
-            if (component != null)
-            {
-                return component;
-            }
-
-            component = FindComponentInChildrenRecursive(child.transform, type);
-            if (component != null)
-            {
-                return component;
-            }
-        }
-
         return null;
     }
 }
-
